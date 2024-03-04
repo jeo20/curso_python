5ef3cfe8689d144a91b9a5e2c8b91385da418ae3
JeO <jeo2k1@gmail.com>
2024-03-04 12:28:28 -0300

    fastapi desde 0

--- 02_intermedio/06_manejo_de_archivos.py
+++ 02_intermedio/06_manejo_de_archivos.py
@@ -10,7 +10,7 @@ import os
 # .txt file
 
 # Leer, escribir y sobrescribir si ya existe
-txt_file = open("01_intermedio/my_file.txt", "w+", encoding="utf-8")
+txt_file = open("02_intermedio/my_file.txt", "w+", encoding="utf-8")
 
 txt_file.write(
     "Mi nombre es Jorge\nMi apellido es Orellana\n45 años\nY mi lenguaje preferido es Python")
@@ -27,7 +27,7 @@ print(txt_file.readline())
 
 txt_file.close()
 
-with open("01_intermedio/my_file.txt", "a") as my_other_file:
+with open("02_intermedio/my_file.txt", "a") as my_other_file:
     my_other_file.write("\nY Visual Fox Pro")
 
 # os.remove("Intermediate/my_file.txt")
@@ -37,7 +37,7 @@ with open("01_intermedio/my_file.txt", "a") as my_other_file:
 
 # .json file
 
-json_file = open("01_intermedio/my_file.json", "w+")
+json_file = open("02_intermedio/my_file.json", "w+")
 
 json_test = {
     "name": "Jorge Eduardo",
@@ -55,11 +55,11 @@ json.dump(json_test, json_file, indent=2)
 
 json_file.close() #  cerrar el fichero una vez terminado de trabajar con él
 
-with open("01_intermedio/my_file.json") as my_other_file:
+with open("02_intermedio/my_file.json") as my_other_file:
     for line in my_other_file.readlines():
         print(line)
 
-json_dict = json.load(open("01_intermedio/my_file.json")) #  Cargamos el contenido del json y lo convertimos en un diccionario
+json_dict = json.load(open("02_intermedio/my_file.json")) #  Cargamos el contenido del json y lo convertimos en un diccionario
 print(json_dict)
 print(type(json_dict))
 print(json_dict["name"])
@@ -67,7 +67,7 @@ print(json_dict["name"])
 
 # .csv file
 
-csv_file = open("01_intermedio/my_file.csv", "w+")
+csv_file = open("02_intermedio/my_file.csv", "w+")
 
 csv_writer = csv.writer(csv_file)
 csv_writer.writerow(["name", "surname", "age", "language", "website"])
@@ -76,7 +76,7 @@ csv_writer.writerow(["Roswell", "", 2, "COBOL", ""])
 
 csv_file.close()
 
-with open("01_intermedio/my_file.csv") as my_other_file:
+with open("02_intermedio/my_file.csv") as my_other_file:
     for line in my_other_file.readlines():
         print(line)
 
