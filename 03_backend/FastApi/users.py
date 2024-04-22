5ad199d63f06e1f9e1be3605a0b7cf98bba7e516
JeO <jeo2k1@gmail.com>
2024-04-22 14:21:10 -0300

    user json

--- 03_backend/FastApi/users.py
+++ 03_backend/FastApi/users.py
@@ -16,15 +16,14 @@ users_list = [User(id=1, name="BraisL", surname="Moure", url="https://moure.dev"
               User(id=2, name="MoureL", surname="Dev", url="https://mouredev.com", age=35),
               User(id=3, name="BraisL", surname="Dahlberg", url="https://haakon.com", age=33)]
        
-# Url local: http://127.0.0.1:8000/users
-@app.get("/usersjson") # decorador  que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
+# Url local: http://127.0.0.1:8000/usersjson
+@app.get("/usersjson") # decorador  que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /usersjson
 async def users(): # esta funcion se ejecutara cuando hagamos una peticion get a /users
     return [{"name": "Brais", "surname": "Moure", "url": "https://moure.dev", "age": 35},
             {"name": "Moure", "surname": "Dev", "url": "https://mouredev.com", "age": 35},
             {"name": "Haakon", "surname": "Dahlberg", "url": "https://haakon.com", "age": 33}]
 
-#url local: http://127.0.0.1:8000/userclass
-@app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /usersclass
+#url local: http://127.0.0.1:8000/users
+@app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
 async def users():  # Creamos un JSON de la lista de usuarios
     return users_list # retorna el contenido de la lista user_list
-#    return User(name ="Brais", surname="Moure", url="https://moure.dev", age=35) # llama a la clase User y retorna el usuario Brais
