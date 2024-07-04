51852fb2e4dbec62a784dd13b86757f3613364e8
JeO <jeo20@hotmail.com>
2024-07-03 23:54:27 -0300

    Merge branch 'main' of https://github.com/jeo20/curso_python

--- 03_backend/FastApi/main.py
+++ 03_backend/FastApi/main.py
@@ -8,8 +8,9 @@ app = FastAPI() # creando instancia de FastAPI llamada app
 async def root(): # definicion de la ruta raiz, es decir, cuando se accede a http://localhost:8000/ (asincrona)
     return {"message": "Hola JeO"} # retornar un json(clave:valor) con el mensaje "Hola JeO"
 
-
+"""
 # Url local: http://127.0.0.1:8000/url
 @app.get("/url")  # decorador @ que indica que la función es una ruta GET http://localhost:8000/url
 async def url(): # definición de la función asincrona url
     return {"url": "https://mouredev.com/python"} # retornar un json con la clave "url" y su valor como URL
+"""
