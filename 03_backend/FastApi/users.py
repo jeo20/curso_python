6acd56711f741df714ad130d9bf868c98a47df59
JeO <jeo2k1@gmail.com>
2024-07-15 15:53:02 -0300

    08 - Creación API

--- 03_backend/FastApi/users.py
+++ 03_backend/FastApi/users.py
@@ -1,10 +1,17 @@
-from fastapi import FastAPI # importando fastapi
-from pydantic import BaseModel #importando la clase BaseModel de pydantic para crear nuestra propia clase
+from fastapi import FastAPI  # importando fastapi
+from pydantic import \
+    BaseModel  # importando la clase BaseModel de pydantic para crear nuestra propia clase
 
 app = FastAPI() # creando instancia de FastAPI llamada app
 
-# Inicia el server: uvicorn users:app --reload
+#url local: http://127.0.0.1:8000/users
+@app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
+async def users():  # Creamos un JSON de la lista de usuarios
+    return "Hola FastAPI"
 
+
+
+"""
 class User(BaseModel): # Creo clase User heredada de BaseModel con 4 atributos (name, surname, url y age)
     name: str
     surname: str
@@ -23,7 +30,9 @@ async def users(): # esta funcion se ejecutara cuando hagamos una peticion get a
             {"name": "Moure", "surname": "Dev", "url": "https://mouredev.com", "age": 35},
             {"name": "Haakon", "surname": "Dahlberg", "url": "https://haakon.com", "age": 33}]
 
+
 #url local: http://127.0.0.1:8000/users
 @app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
 async def users():  # Creamos un JSON de la lista de usuarios
     return users_list # retorna el contenido de la lista user_list
+"""
