0dc36674911b68deba956f4ff8567ad0de98c4c4
JeO <jeo2k1@gmail.com>
2024-07-16 15:53:15 -0300

    hasta 08 - Creación API

--- 03_backend/FastApi/users.py
+++ 03_backend/FastApi/users.py
@@ -2,16 +2,11 @@ from fastapi import FastAPI  # importando fastapi
 from pydantic import \
     BaseModel  # importando la clase BaseModel de pydantic para crear nuestra propia clase
 
+# uvicorn users:app --reload
+
 app = FastAPI() # creando instancia de FastAPI llamada app
 
-#url local: http://127.0.0.1:8000/users
-@app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
-async def users():  # Creamos un JSON de la lista de usuarios
-    return "Hola FastAPI"
-
-
-
-"""
+# Entidad User
 class User(BaseModel): # Creo clase User heredada de BaseModel con 4 atributos (name, surname, url y age)
     name: str
     surname: str
@@ -22,17 +17,15 @@ class User(BaseModel): # Creo clase User heredada de BaseModel con 4 atributos (
 users_list = [User(id=1, name="BraisL", surname="Moure", url="https://moure.dev", age=35),
               User(id=2, name="MoureL", surname="Dev", url="https://mouredev.com", age=35),
               User(id=3, name="BraisL", surname="Dahlberg", url="https://haakon.com", age=33)]
-       
-# Url local: http://127.0.0.1:8000/usersjson
-@app.get("/usersjson") # decorador  que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /usersjson
-async def users(): # esta funcion se ejecutara cuando hagamos una peticion get a /users
+
+#url local: http://127.0.0.1:8000/users
+@app.get("/usersjson") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
+async def usersjson():  # Creamos un JSON de la lista de usuarios
     return [{"name": "Brais", "surname": "Moure", "url": "https://moure.dev", "age": 35},
             {"name": "Moure", "surname": "Dev", "url": "https://mouredev.com", "age": 35},
             {"name": "Haakon", "surname": "Dahlberg", "url": "https://haakon.com", "age": 33}]
-
-
+    
 #url local: http://127.0.0.1:8000/users
 @app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
 async def users():  # Creamos un JSON de la lista de usuarios
     return users_list # retorna el contenido de la lista user_list
-"""
