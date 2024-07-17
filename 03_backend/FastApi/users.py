7cf29fb0c0e4a64d9bc88f236eb5b4bebf46df9b
JeO <jeo2k1@gmail.com>
2024-07-17 15:31:37 -0300

    hasta 09 - Path y Query

--- 03_backend/FastApi/users.py
+++ 03_backend/FastApi/users.py
@@ -8,6 +8,7 @@ app = FastAPI() # creando instancia de FastAPI llamada app
 
 # Entidad User
 class User(BaseModel): # Creo clase User heredada de BaseModel con 4 atributos (name, surname, url y age)
+    id: int
     name: str
     surname: str
     url: str
@@ -16,7 +17,7 @@ class User(BaseModel): # Creo clase User heredada de BaseModel con 4 atributos (
 # Creo una lista de usuarios
 users_list = [User(id=1, name="BraisL", surname="Moure", url="https://moure.dev", age=35),
               User(id=2, name="MoureL", surname="Dev", url="https://mouredev.com", age=35),
-              User(id=3, name="BraisL", surname="Dahlberg", url="https://haakon.com", age=33)]
+              User(id=3, name="Jorge", surname="Orellana", url="https://jeo.com", age=46)]
 
 #url local: http://127.0.0.1:8000/users
 @app.get("/usersjson") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
@@ -29,3 +30,22 @@ async def usersjson():  # Creamos un JSON de la lista de usuarios
 @app.get("/users") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /users
 async def users():  # Creamos un JSON de la lista de usuarios
     return users_list # retorna el contenido de la lista user_list
+
+#Path: http://127.0.0.1:8000/user/id
+@app.get("/user/{id}") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /user/id
+async def user(id:int):  # Creamos un JSON con el id definido en path
+    return search_user(id)
+    
+
+#Query: http://127.0.0.1:8000/userquery/
+@app.get("/userquery/") # decorador que indica a FastAPI que este es un metodo GET y que va a estar en la ruta /userquery/
+async def user(id:int):  # Creamos un JSON con el id definido en path
+    return search_user(id)
+    
+# Funcion que busca el id del usuario
+def search_user(id:int):
+    users = filter(lambda user: user.id == id, users_list) # filtro para encontrar el id especifico con funcion lambda
+    try:
+        return list(users)[0] # devuelve el id buscado en el path
+    except:
+        return {"Error": "Usuario no encontrado"} # mensaje error de excepcion 
