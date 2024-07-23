6d6e70f6039f4b934c167856b399bb2db60d04d3
JeO <jeo2k1@gmail.com>
2024-07-23 15:53:00 -0300

    hasta 17 - MongoDB Atlas

--- 03_backend/FastApi/db/client.py
+++ 03_backend/FastApi/db/client.py
@@ -1,4 +1,7 @@
 from pymongo import MongoClient
 
-db_client = MongoClient() # si no le paso parametro se conecta a localhost
+#Base de datos local
+#db_client = MongoClient().local # si no le paso parametro se conecta a localhost
 
+#Base de datos remota
+db_client = MongoClient('mongodb+srv://jeo2k1:Python2024@cluster0.euzcrke.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').test
